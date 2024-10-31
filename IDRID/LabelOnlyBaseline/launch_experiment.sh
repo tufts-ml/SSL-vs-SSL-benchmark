6f1af8c7aca2c86fea78dbe4bddfbea00d5cd22f
Lakshita <lak.jai110@gmail.com>
2024-10-31 17:50:47 -0400

    Update dataset paths in launch_experiment.sh to reflect IDRID directory structure

--- IDRID/LabelOnlyBaseline/launch_experiment.sh
+++ IDRID/LabelOnlyBaseline/launch_experiment.sh
@@ -43,9 +43,9 @@ export start_epoch=0
 
 
 #data paths
-export l_train_dataset_path='src.LabelOnlyBaseline.train_data.npy'
-export val_dataset_path='src.LabelOnlyBaseline.val_data.npy'
-export test_dataset_path='src.LabelOnlyBaseline.test_data.npy'
+export l_train_dataset_path='src.IDRID.LabelOnlyBaseline.train_data.npy'
+export val_dataset_path='src.IDRID.LabelOnlyBaseline.val_data.npy'
+export test_dataset_path='src.IDRID.LabelOnlyBaseline.test_data.npy'
 
 #shared config
 export labeledtrain_batchsize=64 #default
