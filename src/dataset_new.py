aae20da5598d1aaf2f41ef09f41da69a1d86eb6d
Lakshita <Lak.Jai110@gmail.com>
2024-11-05 15:36:53 -0500

    Update imports

--- src/dataset_new.py
+++ src/dataset_new.py
@@ -1,9 +1,8 @@
 import os
 import torch
 import pandas as pd
-from skimage import io, transform
+from skimage import io
 import numpy as np
-import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
