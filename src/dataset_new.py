23047528c4ee5709d7f3028f34850e0bf1c09950
Lakshita <Lak.Jai110@gmail.com>
2024-11-05 15:27:04 -0500

    Create new dataset loader

---
+++ src/dataset_new.py
@@ -0,0 +1,34 @@
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+class data(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.labels = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            index = index.tolist()
+
+        img_name = os.path.join(self.root_dir, self.labels.iloc[index, 0])
+        image = io.imread(img_name)
+
+        #TODO: Update this to work for multi-class label data
+        labels = self.labels.iloc[index, 1] 
+        labels = np.array([labels], dtype=np.float32)
+        sample = {'image': image, 'label': labels}
+
+        if self.transform:
+            sample = self.transform(sample)
+        
+        return sample
