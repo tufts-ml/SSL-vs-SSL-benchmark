37742aa9f7198ade2cee7ce090cc2d82fce0f32c
Lakshita <Lak.Jai110@gmail.com>
2024-11-08 22:22:24 -0500

    Update launch_experiment.sh

--- runs/IDRID/LabelOnlyBaseline/launch_experiment.sh
+++ runs/IDRID/LabelOnlyBaseline/launch_experiment.sh
@@ -17,8 +17,8 @@ else
 fi
 
 export resized_shape=384
-export num_workers=0
-export total_hour=1
+export num_workers=20
+export total_hour=25
 export num_classes=5
 export use_pretrained='False'
 export patience=20
@@ -29,17 +29,18 @@ export resume='None'
 
 #experiment setting
 export dataset_name='IDRID'
-export data_seed=0
+export data_seed=2
 export training_seed=0
 
-export train_dir="/cluster/tufts/hugheslab/ljain01/experiments/LABELONLYBASELINE"
+export train_dir="/cluster/tufts/hugheslab/sslbench/experiments/$dataset_name/data_seed$data_seed/training_seed$training_seed/$implementation"
+
 mkdir -p $train_dir
 
 export script='src.LabelOnlyBaseline.LabelOnlyBaseline'
 
 
 export arch='resnet18'
-export train_epoch=50
+export train_epoch=200
 export start_epoch=0
 
 
@@ -50,7 +51,7 @@ export test_dataset_path='/cluster/tufts/hugheslab/datasets/IDRID/Labels/test.cs
 export root_dataset_path='/cluster/tufts/hugheslab/datasets/IDRID/Images'
 
 #shared config
-export labeledtrain_batchsize=16 
+export labeledtrain_batchsize=32 
 
 #PL config, candidate hypers to search
 export optimizer_type='Adam'
@@ -62,10 +63,10 @@ export lr_cycle_epochs=$train_epoch
 
 
 if [[ $ACTION_NAME == 'submit' ]]; then
-    ## Use this line to submit the experiment to the batch scheduler
-    sbatch <./do_experiment.slurm
+    ## Submit the experiment with the correct resources for batch scheduler
+    sbatch --time=0-02:00 --mem=30G --gres=gpu:rtx_6000:2 --cpus-per-task=30 -p hugheslab <./do_experiment.slurm
 
 elif [[ $ACTION_NAME == 'run_here' ]]; then
-    ## Use this line to just run interactively
-    bash ./do_experiment.slurm
+    ## Run the experiment interactively with the correct resources
+    srun --time=0-02:00 --mem=30G --gres=gpu:rtx_6000:2 --cpus-per-task=30 -p hugheslab --pty bash ./do_experiment.slurm
 fi
