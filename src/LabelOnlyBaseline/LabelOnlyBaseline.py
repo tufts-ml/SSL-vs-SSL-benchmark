658e3041a6b86d39919e4eb35ad76387393a32e4
NikkiFalicov <nicole.falicov@gmail.com>
2024-11-14 14:20:06 -0500

    Added CLAHE normalization to image transformation

--- src/LabelOnlyBaseline/LabelOnlyBaseline.py
+++ src/LabelOnlyBaseline/LabelOnlyBaseline.py
@@ -29,7 +29,8 @@ from torchvision import transforms
 from torch.utils.tensorboard import SummaryWriter
 
 
-from src.dataset_csv import ImageCSVDataset
+from src.dataset_csv import ImageCSVDataset 
+from src.tmed2_clahe import apply_clahe
 from src.LabelOnlyBaseline.libml.utils import save_pickle
 from src.LabelOnlyBaseline.libml.utils import train_one_epoch, eval_model
 from src.LabelOnlyBaseline.libml.utils import EarlyStopping
@@ -214,7 +215,8 @@ def main(args):
 
     transform_labeledtrain = transforms.Compose([
         # transforms.ToPILImage(),
-        # transforms.Grayscale(num_output_channels=3),
+        transforms.Lambda(apply_clahe),
+        transforms.Grayscale(num_output_channels=3),
         transforms.RandomHorizontalFlip(),
         transforms.RandomCrop(size=image_size,
                               padding=int(image_size*0.125),
@@ -225,7 +227,8 @@ def main(args):
 
     transform_eval = transforms.Compose([
         # transforms.ToPILImage(),
-        # transforms.Grayscale(num_output_channels=3),
+        transforms.Lambda(apply_clahe),
+        transforms.Grayscale(num_output_channels=3),
         transforms.ToTensor(),
         transforms.Normalize(mean=dataset_mean, std=dataset_std)
     ])
