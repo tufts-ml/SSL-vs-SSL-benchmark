f2b3d06189da9f5bbf17cf3e4e7dcacf06af35eb
PatrickFeeney <patrickfeeney1225@gmail.com>
2024-11-15 09:39:04 -0500

    Streamline Loading and Add Doc Template

--- src/dataset_csv.py
+++ src/dataset_csv.py
@@ -1,39 +1,42 @@
 import os
 import torch
 import pandas as pd
-from skimage import io
-import numpy as np
 from torchvision.datasets import VisionDataset
 from PIL import Image
 
 
 class ImageCSVDataset(VisionDataset):
     def __init__(self, csv_file, root_dir, transforms=None, transform=None, target_transform=None):
+        """_summary_
+
+        Args:
+            csv_file (_type_): _description_
+            root_dir (_type_): _description_
+            transforms (_type_, optional): _description_. Defaults to None.
+            transform (_type_, optional): _description_. Defaults to None.
+            target_transform (_type_, optional): _description_. Defaults to None.
+        """
         super().__init__(root=root_dir, transforms=transforms, transform=transform,
                          target_transform=target_transform)
-        # TODO: rename labels later
-        self.data = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
 
+        dataframe = pd.read_csv(csv_file)
+        self.img_paths = dataframe.iloc[:, 0]
+        self.labels = dataframe.iloc[:, 1]
+
     def __len__(self):
-        # Using shape[0] for explicit row count
-        return self.data.shape[0]
+        return len(self.img_paths)
 
     def __getitem__(self, index):
         if torch.is_tensor(index):
             index = index.item()
 
-        img_name = os.path.join(self.root_dir, self.data.iloc[index, 0])
-        image = io.imread(img_name)
-        
-        # Convert the image to PIL format if it’s not already
-        if isinstance(image, np.ndarray):
-            image = Image.fromarray(image)
-
-        # Assuming single-class label at index 1
-        label = self.data.iloc[index, 1]
-        label = np.int32(label)
+        # Load image from file
+        img_name = os.path.join(self.root_dir, self.img_paths[index])
+        image = Image.open(img_name)
+        # Select label
+        label = int(self.labels[index])
 
         # Return as a tuple (image, label) for compatibility
         if self.transforms is not None:
