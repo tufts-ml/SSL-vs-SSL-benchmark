dce6d81d3af05f5124dfdcfc86120da2846f08ba
NikkiFalicov <nicole.falicov@gmail.com>
2025-01-30 20:52:06 -0500

    Fixed TODO comments

--- src/dataset_csv.py
+++ src/dataset_csv.py
@@ -28,64 +28,58 @@ class ImageCSVDataset(VisionDataset):
         if isinstance(image, np.ndarray):
             image = Image.fromarray(image)
 
-        # Assuming single-class label at index 1
-        label = self.data.iloc[index, 1]
-        label = np.int32(label)
+        rows, cols = self.data.shape
 
-        # Return as a tuple (image, label) for compatibility
-        if self.transforms is not None:
-            return self.transforms(image, label)
-           
-        return (image, label)
+        # Working with labeled data
+        if cols > 1:
+            # Assuming single-class label at index 1
+            label = self.data.iloc[index, 1]
+            label = np.int32(label)
+
+            # Return as a tuple (image, label) for compatibility
+            if self.transforms is not None:
+                return self.transforms(image, label)
+
+            return (image, label)
+
+        # Working with unlabeled data
+        else:
+            if self.transforms is not None:
+                return self.transforms(image)
+
+            return image
 
 
 class UnlabeledImageCSVDataset(ImageCSVDataset):
     def __init__(self, csv_file, root_dir, transforms=None, transform=None, target_transform=None):
         super().__init__(csv_file, root_dir, transforms, transform, target_transform)
 
-    # TODO can the below two functions be inherited or use super function calls?
-    # should be possible if the CSV is still expected to have 2 columns
     def __len__(self):
-        return len(self.data)
+        super().__len__()
 
     def __getitem__(self, index):
-        if torch.is_tensor(index):
-            index = index.item()
-
-        img_name = os.path.join(self.root_dir, self.data.iloc[index, 0])
-        image = io.imread(img_name)
-
-        # Convert the image to PIL format if it’s not already
-        if isinstance(image, np.ndarray):
-            image = Image.fromarray(image)
-
-        # Return data
-        if self.transforms is not None:
-            return self.transforms(image)
-
-        return image
+        super().__getitem__(index)
 
 
 class CheXpertDataset(VisionDataset):
-    # TODO how could inheritance reduce the amount of code in these first 2 functions?
     def __init__(self, csv_file, root_dir, transforms=None, transform=None, target_transform=None):
         super().__init__(root=root_dir, transforms=transforms, transform=transform,
                          target_transform=target_transform)
-        self.labels = pd.read_csv(csv_file)
+        self.data = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
         self.label_columns = ['Edema', 'Consolidation', 'Atelectasis', 'Pneumothorax', 'Pleural Effusion']
 
 
-    def __len__(self):
-        return self.labels.shape[0]
+    def __len__(self): 
+        return self.data.shape[0]
 
 
     def __getitem__(self, index):
         if torch.is_tensor(index):
             index = index.item()
 
-        img_path = self.labels.iloc[index, 0]
+        img_path = self.data.iloc[index, 0]
         img_name = os.path.join(self.root_dir, img_path)
 
         if not os.path.exists(img_name):
@@ -93,7 +87,7 @@ class CheXpertDataset(VisionDataset):
             return None, None
 
         image = io.imread(img_name)
-        labels = self.labels.loc[index, self.label_columns]
+        labels = self.data.loc[index, self.label_columns]
         labels = labels.fillna(0)  # Replace NaN values with 0
         labels = labels.astype(np.int32).values
 
