b88725eb88986447836a5e27dd681765fd58a624
Lakshita <Lak.Jai110@gmail.com>
2025-02-12 16:44:57 -0500

    Fix --use_pretrained

--- src/methods/LabelOnlyBaseline.py
+++ src/methods/LabelOnlyBaseline.py
@@ -6,7 +6,7 @@ class LabelOnlyBaseline(MethodWrapper):
     def forward(self, l_data, l_labels, u_data=None):
         # Forward pass
         logits = self.backbone(l_data)
-        
+
         s_loss = func.cross_entropy(logits, l_labels, weight=self.args.weights, reduction='mean')
-        
+
         return logits, s_loss, s_loss, 0  # No unsupervised loss
