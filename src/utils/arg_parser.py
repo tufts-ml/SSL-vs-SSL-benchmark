cae72a926edf33c7f6b78149e98110e87c8716bb
PatrickFeeney <patrickfeeney1225@gmail.com>
2025-02-18 15:36:19 -0500

    Argparse strs to bools

--- src/utils/arg_parser.py
+++ src/utils/arg_parser.py
@@ -22,8 +22,8 @@ def parse_args():
     parser.add_argument('--implementation', default='', type=str,)
     parser.add_argument('--arch', default='resnet18', type=str, help='backbone to use')
     # pretrained weights for resnet18
-    parser.add_argument('--use_pretrained', default='False', type=str)
-    parser.add_argument('--freeze_backbone', default='True', type=str)
+    parser.add_argument('--use_pretrained', action="store_true")
+    parser.add_argument('--freeze_backbone', action="store_true")
 
     # training process settings
     parser.add_argument('--train_epoch', default=300, type=int, help='total epochs to run')
