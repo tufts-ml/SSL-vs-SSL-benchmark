f047f7213b8b0b6ed65500d147b0279114b449bf
Lakshita <Lak.Jai110@gmail.com>
2025-04-01 09:44:55 -0400

    Remove redundant 'l_' prefix from root dataset path arguments in launch scripts

--- runs/IDRID/LabelOnlyBaseline/launch_lp.py
+++ runs/IDRID/LabelOnlyBaseline/launch_lp.py
@@ -25,7 +25,7 @@ launch_args = [
     "--l_train_dataset_path /cluster/tufts/hugheslab/datasets/IDRID/Labels/train.csv",
     "--val_dataset_path /cluster/tufts/hugheslab/datasets/IDRID/Labels/validation.csv",
     "--test_dataset_path /cluster/tufts/hugheslab/datasets/IDRID/Labels/test.csv",
-    "--l_root_dataset_path /cluster/tufts/hugheslab/datasets/IDRID/Images",
+    "--root_dataset_path /cluster/tufts/hugheslab/datasets/IDRID/Images",
 
     # DataLoader config
     "--labeledtrain_batchsize 32",
