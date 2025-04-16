5f5ab63dadbd495b89766a674e9d7460d33fbcc7
NikkiFalicov <nicole.falicov@gmail.com>
2025-04-16 12:20:17 -0400

    Added in base for new transformation handling

--- src/ssl_bench/config.py
+++ src/ssl_bench/config.py
@@ -1,4 +1,7 @@
 from scipy.stats import loguniform
+from torchvision import transforms
+from torchvision.models import ResNet18_Weights
+from ssl_bench.utils.apply_clahe import apply_clahe
 
 dataset_configs = {
     'TissueMNIST': {'dataset_mean': (0.0988, 0.0988, 0.0988),
@@ -39,6 +42,181 @@ dataset_configs = {
                  'nimg_per_epoch': 3500,
                  'num_classes': 2},
 }
+
+def get_transformations(args):
+    dataset_name = args.dataset_name
+    method = args.implementation
+
+    dataset_mean = dataset_configs[dataset_name]['dataset_mean']
+    dataset_std = dataset_configs[dataset_name]['dataset_std']
+    image_size = dataset_configs[dataset_name]['image_size']
+    
+    # dealing with issue in transformation dictionary for methods
+    # expecting a single value when image_size is a tuple
+    if isinstance(image_size, tuple):
+        square_img_size = image_size[0]
+    else:
+        square_img_size = image_size
+
+    dataset_base_transformations = {
+        'TMED2': {
+            "l_train": transforms.Compose([
+                transforms.Grayscale(num_output_channels=3),
+                transforms.Lambda(apply_clahe),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomCrop(size=image_size,
+                                        padding=int(square_img_size*0.125),
+                                        padding_mode='reflect'),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=dataset_mean, std=dataset_std)
+            ]),
+            "u_train": None,
+            "val": transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Grayscale(num_output_channels=3),
+                transforms.RandomHorizontalFlip(),
+                transforms.Resize(400),
+                transforms.CenterCrop(320),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=dataset_mean, std=dataset_std)
+            ]),
+            "test": transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Grayscale(num_output_channels=3),
+                transforms.RandomHorizontalFlip(),
+                transforms.Resize(400),
+                transforms.CenterCrop(320),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=dataset_mean, std=dataset_std)
+            ])
+        },
+        'IDRID': {
+            "l_train": transforms.Compose([
+                transforms.Resize(size=image_size),
+                transforms.Lambda(apply_clahe),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomCrop(size=image_size,
+                                    padding=int(square_img_size*0.125),
+                                    padding_mode='reflect'),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=dataset_mean, std=dataset_std)
+            ]),
+            "u_train": None,
+            "val": transforms.Compose([
+                transforms.Resize(size=image_size),
+                transforms.Lambda(apply_clahe),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=dataset_mean, std=dataset_std)
+            ]),
+            "test": transforms.Compose([
+                transforms.Resize(size=image_size),
+                transforms.Lambda(apply_clahe),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=dataset_mean, std=dataset_std)
+            ])
+        }, 
+        'CheXpert': {
+            "l_train": transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Grayscale(num_output_channels=3),
+                transforms.RandomHorizontalFlip(),
+                transforms.Resize(400),
+                transforms.CenterCrop(320),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=dataset_mean, std=dataset_std)
+            ]),
+            "u_train": None,
+            "val": transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Grayscale(num_output_channels=3),
+                transforms.RandomHorizontalFlip(),
+                transforms.Resize(400),
+                transforms.CenterCrop(320),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=dataset_mean, std=dataset_std)
+            ]),
+            "test": transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Grayscale(num_output_channels=3),
+                transforms.RandomHorizontalFlip(),
+                transforms.Resize(400),
+                transforms.CenterCrop(320),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=dataset_mean, std=dataset_std)
+            ])
+        }
+    }
+
+
+    method_transformations = {
+        "Pretrained_IDRID": {
+            "l_train": transforms.Compose([
+                transforms.Grayscale(num_output_channels=3),
+                ResNet18_Weights.IMAGENET1K_V1.transforms(),
+            ]),
+            "u_train": None,
+            "val": transforms.Compose([
+                transforms.Grayscale(num_output_channels=3),
+                ResNet18_Weights.IMAGENET1K_V1.transforms(),
+            ]),
+            "test": transforms.Compose([
+                transforms.Grayscale(num_output_channels=3),
+                ResNet18_Weights.IMAGENET1K_V1.transforms(),
+            ])
+        },
+        "LabelOnlyBaseline": {
+            "l_train": None,
+            "u_train": None,
+            "val": None,
+            "test": None
+        },
+        "MixUp": {
+            "l_train": None,
+            "u_train": None,
+            "val": None,
+            "test": None
+        }
+    }
+
+    if dataset_name not in dataset_base_transformations:
+        raise NotImplementedError(f"Must add in base transformations for dataset {dataset_name}")
+    l_train_transform, u_train_transform, val_transform, test_transform = None, None, None, None
+    pretrained_idrid = args.use_pretrained and (dataset_name == "IDRID")
+
+    if (method in method_transformations) or pretrained_idrid:
+        if pretrained_idrid:
+            entry = method_transformations["Pretrained_IDRID"]
+        else:
+            entry = method_transformations[method]
+
+        
+        
+        # add logic here for transforming unlabeled data
+        if entry["u_train"] is None:
+            u_train_transform = dataset_base_transformations[dataset_name]["u_train"]
+        else:
+            if method == "BarlowTwins":
+                pass
+
+        if entry["l_train"] is None:
+            l_train_transform = dataset_base_transformations[dataset_name]["l_train"]
+        else:
+            l_train_transform = entry["l_train"]
+
+        if entry["val"] is None:
+            val_transform = dataset_base_transformations[dataset_name]["val"]
+        else:
+            val_transform = entry['val']
+
+        if entry["test"] is None:
+            test_transform = dataset_base_transformations[dataset_name]["test"]
+        else:
+            test_transform = entry['test']
+    else:
+        raise NotImplementedError(f"Must add in transformations for method: {method}")
+
+    breakpoint()
+    return l_train_transform, u_train_transform, val_transform, test_transform
 
 
 class HyperparamSpace():
