e9337f121acf94e9e21a4e0c43570488faac8bf3
Lakshita <Lak.Jai110@gmail.com>
2025-04-25 09:20:42 -0400

    Fixed evaluation device logic

--- runs/IDRID/BarlowTwins/launch_lp.py
+++ runs/IDRID/BarlowTwins/launch_lp.py
@@ -40,7 +40,7 @@ launch_args = [
     "--arch resnet18",
 
     # output location
-    "--train_dir /cluster/tufts/hugheslab/sslbench/experiments/IDRID/BarlowTwins/log_reg/fft",
+    "--train_dir /cluster/tufts/hugheslab/sslbench/experiments/IDRID/BarlowTwins/noaug/fft",
 
     # optimization config
     "--train_epoch 100",
