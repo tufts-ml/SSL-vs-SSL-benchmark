31decf47ef9bfee72aa67b3456ba6c9bf378e265
Lakshita <Lak.Jai110@gmail.com>
2025-04-25 10:07:14 -0400

    Add SimCLR model to implementation map in get_model function

--- src/ssl_bench/utils/train_utils.py
+++ src/ssl_bench/utils/train_utils.py
@@ -11,6 +11,7 @@ import torch.nn as nn
 from ssl_bench.methods.LabelOnlyBaseline import LabelOnlyBaseline
 from ssl_bench.methods.MixUp import MixUp
 from ssl_bench.methods.BarlowTwins import BarlowTwins
+from ssl_bench.methods.SimCLR import SimCLR
 import torch.optim as optim
 
 
@@ -163,6 +164,7 @@ def get_model(args):
         'LabelOnlyBaseline': LabelOnlyBaseline,
         'MixUp': MixUp,
         'BarlowTwins': BarlowTwins,
+        'SimCLR': SimCLR,
     }
 
     model_class = implementation_map.get(args.implementation)
